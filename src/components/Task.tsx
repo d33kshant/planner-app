9233d0a1cd7541d361da911dd2ab5ee02fcd54cc
Deekshant Yadav <d33kshant@gmail.com>
2022-01-27 00:32:05 +0530

    Updated Task to be Draggable

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react"
+import { Draggable } from "react-beautiful-dnd";
 import "../styles/Task.css"
 
 type Props = {
 	value: string,
+	draggableId: number,
 }
 
-const Task: React.FC<Props> = ({ value }) => {
+const Task: React.FC<Props> = ({ value, draggableId }) => {
 
 	const [taskBody, setTaskBody] = useState<string>(value);
 
@@ -16,23 +18,27 @@ const Task: React.FC<Props> = ({ value }) => {
 	}
 
 	return (
-		<div className="task-container">
-			{/* <div className="labels-list">
-				<div className="label-card label-green"></div>
-				<div className="label-card label-blue"></div>
-			</div> */}
-			<span 
-				className="task-body"
-				contentEditable={true}
-				suppressContentEditableWarning={true}
-				onBlur={(event)=>{
-					onSpanBlur(event)
-				}}
-				defaultValue={taskBody}
-			>
-				{taskBody}
-			</span>
-		</div>
+		<Draggable draggableId={draggableId.toString()} index={draggableId}>
+			{(provided)=>(
+				<div className="task-container" ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+					{/* <div className="labels-list">
+						<div className="label-card label-green"></div>
+						<div className="label-card label-blue"></div>
+					</div> */}
+					<span 
+						className="task-body"
+						contentEditable={true}
+						suppressContentEditableWarning={true}
+						onBlur={(event)=>{
+							onSpanBlur(event)
+						}}
+						defaultValue={taskBody}
+					>
+						{taskBody}
+					</span>
+				</div>
+			)}
+		</Draggable>
 	)
 }
 
