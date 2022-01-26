13aaf3be24d50d408db795cec24d5c8ef6f66e3f
Deekshant Yadav <d33kshant@gmail.com>
2022-01-26 16:34:36 +0530

    Added feature to display and modify task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import "../styles/Task.css"
 
-const Task: React.FC = () => {
+type Props = {
+	value: string,
+}
+
+const Task: React.FC<Props> = ({ value }) => {
+
+	const [taskBody, setTaskBody] = useState<string>(value);
+
+	const onSpanBlur: Function = (event: React.FocusEvent<HTMLSpanElement>) => {
+		const text = event.currentTarget.innerText
+		if(text) setTaskBody(text)
+		else setTaskBody(taskBody)
+	}
+
 	return (
 		<div className="task-container">
-			<button className="task-edit-button">
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"/></svg>
-			</button>
-			<div className="labels-list">
+			{/* <div className="labels-list">
 				<div className="label-card label-green"></div>
 				<div className="label-card label-blue"></div>
-			</div>
-			<span className="task-body">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum libero tempora obcaecati asperiores repellendus repudiandae.
+			</div> */}
+			<span 
+				className="task-body"
+				contentEditable={true}
+				suppressContentEditableWarning={true}
+				onBlur={(event)=>{
+					onSpanBlur(event)
+				}}
+				defaultValue={taskBody}
+			>
+				{taskBody}
 			</span>
 		</div>
 	)
