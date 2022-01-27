85b4735fec64ff98d428f81f87c7e8d31d91b991
Deekshant Yadav <d33kshant@gmail.com>
2022-01-27 23:08:06 +0530

    Fixed board's renaming feature

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,20 +1,22 @@
-import React, { useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import Task from "./Task"
 import "../styles/Board.css"
 import AddCard from "./AddCard"
 import { Droppable } from "react-beautiful-dnd"
 import TaskItem from "../types/TaskItem"
+import { BoardProps } from "../types/Props"
 
-type Props = {
-	title: string
-}
-
-const Board: React.FC<Props> = ({ title }) => {
+const Board: React.FC<BoardProps> = ({ title, droppableId }) => {
 
 	const [boardTitle, setBoardTitle] = useState<string>(title);
 	const [isAddingTask, setIsAddingtask] = useState<boolean>(false);
-
 	const [tasks, setTasks] = useState<TaskItem[]>([]);
+
+	const boardTitleRef = useRef<HTMLSpanElement>(null);
+
+	useEffect(()=>{
+		boardTitleRef.current?.focus()
+	}, [])
 
 	const onBoardTitleEdited: Function = (event: React.FocusEvent<HTMLSpanElement>) => {
 		const newTitle = event.currentTarget.innerText
@@ -34,12 +36,12 @@ const Board: React.FC<Props> = ({ title }) => {
 	return (
 		<div className="board-container" >
 			<div className="board-header">
-				<span className="board-title" contentEditable={true} suppressContentEditableWarning={true} onBlur={(event) => onBoardTitleEdited(event)} >{boardTitle}</span>
+				<span className="board-title" ref={boardTitleRef} contentEditable={true} suppressContentEditableWarning={true} onBlur={(event) => onBoardTitleEdited(event)} >{boardTitle}</span>
 				<button className="header-option-button">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="1" /><circle cx="19" cy="12" r="1" /><circle cx="5" cy="12" r="1" /></svg>
 				</button>
 			</div>
-			<Droppable droppableId={title}>
+			<Droppable droppableId={droppableId.toString()}>
 				{(provided) =>
 				<div className="tasks-list" ref={provided.innerRef} {...provided.droppableProps}>
 					{tasks.map((task, index) => <Task draggableId={task.id} draggableIndex={index} key={index} value={task.body} labels={task.labels} />)}
