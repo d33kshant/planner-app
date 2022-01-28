44377987740c931e6489b147f5c81000fdf0f3eb
Deekshant Yadav <d33kshant@gmail.com>
2022-01-28 16:56:07 +0530

    Updated actions callback

--- src/components/AddCard.tsx
+++ src/components/AddCard.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import "../styles/AddCard.css"
-import TaskItem from "../types/TaskItem";
 
 type Props = {
-	onAddClicked: (task: TaskItem) => void,
+	onAddClicked: (body: string, labels: string[]) => void,
 	onCacelCliced: () => void,
 }
 
@@ -35,7 +34,7 @@ const AddCard: React.FC<Props> = ({ onAddClicked, onCacelCliced })=>{
 				{/* <button className="new-card-button">Add Card</button> */}
 			</form>
 			<div className="card-form-actions">
-				<button className="form-action action-add" onClick={() => onAddClicked({ id: Date.now(), labels: labels, body: inputRef.current?.innerText || "" })}>
+				<button className="form-action action-add" onClick={() => onAddClicked(inputRef.current?.innerText || "", labels)}>
 					<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
 					Add
 				</button>
